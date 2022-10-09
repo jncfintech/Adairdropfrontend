73729a4656034e291dc10312cd3a4c364ed278ce
jncfintech <115144435+jncfintech@users.noreply.github.com>
2022-10-09 15:29:45 +0530

    before cahngges

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const instance =  axios.create({
         // baseURL: 'http://localhost:8000',
         // baseURL: 'https://59e8-125-63-100-177.in.ngrok.io/'
-        baseURL: 'http://35.78.65.206:80'
+        baseURL: 'http://35.78.65.206'
+        // baseURL: 'https://api.zepverse.org'
         
 });
 
